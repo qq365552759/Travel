9dda2e3065aeb3450e47b7935dfe46ff060592ec
vigoss365552759 <365552759@qq.com>
2018-10-29 11:51:22 +0800

    ajax city

--- travel/src/pages/city/components/List.vue
+++ travel/src/pages/city/components/List.vue
@@ -2,7 +2,7 @@
   <div class="list" ref="wrapper">
     <div>
       <div class="area">
-        <div class="tittle border-topbottom">当前城市</div>
+        <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
           <div class="button-wrapper">
             <div class="button">湛江</div>
@@ -10,71 +10,34 @@
         </div>
       </div>
       <div class="area">
-        <div class="tittle border-topbottom">热门城市</div>
-      </div>
+        <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">湛江</div>
-        </div>
-        </div>
-      <div class="area">
-        <div class="tittle border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
+          <div
+            class="button-wrapper"
+            v-for="item of hot"
+            :key="item.id"
+            @click="handleCityClick(item.name)"
+          >
+            <div class="button">{{item.name}}</div>
+          </div>
         </div>
       </div>
-      <div class="area">
-        <div class="tittle border-topbottom">A</div>
+      <div
+        class="area"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key"
+      >
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="tittle border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
-          <div class="item border-bottom">阿拉尔</div>
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id"
+            @click="handleCityClick(innerItem.name)"
+          >
+            {{innerItem.name}}
+          </div>
         </div>
       </div>
     </div>
@@ -85,6 +48,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
