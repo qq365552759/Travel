9df01955912188cc2cb427c3f6b755c617209556
vigoss365552759 <365552759@qq.com>
2018-10-29 17:17:38 +0800

    perfomance

--- travel/src/pages/city/components/List.vue
+++ travel/src/pages/city/components/List.vue
@@ -50,10 +50,19 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
