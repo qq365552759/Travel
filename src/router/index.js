d44f74a6974438b7d5058a094e368670372e28c5
vigoss365552759 <365552759@qq.com>
2018-11-11 13:23:58 +0800

    finish project

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
@@ -8,21 +7,20 @@ import Detail from '@/pages/detail/Detail'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }, {
-      path: '/city',
-      name: 'City',
-      component: City
-    }, {
-      path: '/detail/:id',
-      name: 'Detail',
-      component: Detail
-    }],
-    scrollBehavior (to, from, savedPosition) {
-      return { x:0, y:0}
-    }
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
+  }, {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail
+  }],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
