8576598bb6d8dadce603f94e733dbc29c4fd79b1
vigoss365552759 <365552759@qq.com>
2018-11-12 16:42:06 +0800

    add finish

--- config/index.js
+++ config/index.js
@@ -6,16 +6,12 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8080',
-        pathRewrite: {
-            '^/api': '/static/mock'
-        }
+        target: 'http://localhost:80',
       }
     },
 
